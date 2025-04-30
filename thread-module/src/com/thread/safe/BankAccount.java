ee9924b59bf2c19ba429f542d98339f4e14b7b31
Zhangyao719 <1258858403@qq.com>
2025-04-30 09:02:07 +0800

    feat: 进程安全与 Lock 锁

--- thread-module/src/com/thread/safe/BankAccount.java
+++ thread-module/src/com/thread/safe/BankAccount.java
@@ -1,39 +1,74 @@
 package com.thread.safe;
 
-import lombok.AllArgsConstructor;
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class BankAccount {
+//     private String cardNo;
+//     private double balance;
+//
+//     // 取款
+//     // public void withdraw(double money) {
+//     // 加锁方式二：synchronized 同步方法
+//     public synchronized void withdraw(double money) {
+//         String name = Thread.currentThread().getName();
+//
+//         // 安全问题：
+//         // if (balance >= money) {
+//         //     System.out.println(name + "取款成功");
+//         //     balance -= money;
+//         //     System.out.println(name + "取款后，账户余额： " + balance);
+//         // } else {
+//         //     System.out.println("余额不足！");
+//         // }
+//
+//         // 加锁方式一：synchronized 同步代码块
+//         // 实例方法使用 this，静态方法使用 类名.class
+//         // synchronized (this) {
+//         //     if (balance >= money) {
+//         //         System.out.println(name + "取款成功");
+//         //         this.balance -= money;
+//         //         System.out.println(name + "取款后，账户余额： " + balance);
+//         //     } else {
+//         //         System.out.println("余额不足！");
+//         //     }
+//         // }
+//     }
+// }
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// 加锁方式三：Lock 锁
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class BankAccount {
     private String cardNo;
     private double balance;
+    private final Lock lk = new ReentrantLock();
 
-    // 取款
+    public BankAccount(String cardNo, double balance) {
+        this.cardNo = cardNo;
+        this.balance = balance;
+    }
+
     public void withdraw(double money) {
         String name = Thread.currentThread().getName();
 
-        // 安全问题：
-        // if (money >= balance) {
-        //     System.out.println(name + "取款成功");
-        //     balance -= money;
-        //     System.out.println(name + "取款后，账户余额： " + balance);
-        // } else {
-        //     System.out.println("余额不足！");
-        // }
-
-        // 加锁方式一：synchronized 同步代码块
-        //
-        synchronized (this) {
+        lk.lock(); // 加锁
+        try {
             if (balance >= money) {
                 System.out.println(name + "取款成功");
-                this.balance -= money;
+                balance -= money;
                 System.out.println(name + "取款后，账户余额： " + balance);
             } else {
                 System.out.println("余额不足！");
             }
+        } finally {
+            lk.unlock(); // 解锁
         }
     }
 }
